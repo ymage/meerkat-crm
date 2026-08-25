2e2b613480290d1870f0d833fb9bbf0bb4385c2f
Ymage <heltem@o2php.com>
2026-08-25 15:54:18 +0200

    fix: register EmploymentHistory in the shared controllers test schema

--- backend/controllers/activity_controller_test.go
+++ backend/controllers/activity_controller_test.go
@@ -28,7 +28,7 @@ func setupRouter() (*gorm.DB, *gin.Engine) {
 	sqlDB, _ := db.DB()
 	sqlDB.SetMaxOpenConns(1)
 
-	db.AutoMigrate(&models.Contact{}, &models.Activity{}, &models.Note{}, models.Relationship{}, models.Reminder{}, models.User{}, models.Webhook{}, models.WebhookDelivery{})
+	db.AutoMigrate(&models.Contact{}, &models.Activity{}, &models.Note{}, models.Relationship{}, models.Reminder{}, models.User{}, models.Webhook{}, models.WebhookDelivery{}, &models.EmploymentHistory{})
 
 	user := models.User{Username: "tester", Password: "password123", Email: "tester@example.com"}
 	if err := db.Create(&user).Error; err != nil {
